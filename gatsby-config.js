b40ebf2332857df61fdd9a8e2834f571b30b440b
sitek94 <msitkowski94@gmail.com>
2021-06-04 21:11:12 +0200

    source data from airtable

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,23 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
+
 module.exports = {
   siteMetadata: {
-    title: "gatsby-airtable-example",
+    title: 'gatsby-airtable-example',
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: 'gatsby-source-airtable',
+      options: {
+        apiKey: process.env.GATSBY_AIRTABLE_API_KEY,
+        tables: [
+          {
+            baseId: `appQL2HrfEBsSWQeV`,
+            tableName: `table-one`,
+          },
+        ]
+      },
+    },
+  ],
 };
