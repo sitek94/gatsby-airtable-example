cad8bfd07cb15b94b84949864781d09db5a33b7a
sitek94 <msitkowski94@gmail.com>
2021-06-05 21:13:44 +0200

    add carousel

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -18,10 +18,47 @@ export default function Carousel() {
       css={css`
         --loader-bar-color: transparent;
         --loader-bar-height: 0;
+
+        img {
+          opacity: 0.2;
+        }
       `}
       fillParent
       bullets={false}
-      media={images.map(src => ({ source: src }))}
-    />
+    >
+      {images.map((src, i) => (
+        <div key={i} data-src={src} className="absolute">
+          <div className="sm:text-center lg:text-left">
+            <h1 className="text-4xl tracking-tight font-extrabold text-gray-100 sm:text-5xl md:text-6xl">
+              <span className="block xl:inline">Konstrukcje</span>{' '}
+              <span className="block text-gray-200 xl:inline">szklane</span>
+            </h1>
+            <p className="mt-3 text-base text-gray-100 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
+              lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
+              fugiat aliqua.
+            </p>
+            <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+              <div className="rounded-md shadow">
+                <a
+                  href="#"
+                  className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+                >
+                  Realizacje
+                </a>
+              </div>
+              <div className="mt-3 sm:mt-0 sm:ml-3">
+                <a
+                  href="#"
+                  className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
+                >
+                  Kalkulator
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </AwesomeSlider>
   );
 }
