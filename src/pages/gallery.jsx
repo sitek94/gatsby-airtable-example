b222102861fb5ebe95b1d2228aebbf41e714c9bc
sitek94 <msitkowski94@gmail.com>
2021-06-06 13:26:10 +0200

    change gallery layout to images-only

--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -12,65 +12,96 @@ import img8 from '../images/fake-8.jpg';
 import img9 from '../images/fake-9.jpg';
 import img10 from '../images/fake-10.jpg';
 import img11 from '../images/fake-11.jpg';
+import img12 from '../images/fake-1.jpg';
 
-const items = [
-  img1,
-  img2,
-  img3,
-  img4,
-  img5,
-  img6,
-  img7,
-  img8,
-  img9,
-  img10,
-  img11,
-].map((src, i) => ({
-  id: i,
-  src,
-  title: `Title ${i + 1}`,
-  subtitle: 'SUBTITLE',
-  desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab doloribus, sequi omnis repudiandae non mollitia laborum molestias tempore laboriosam!',
-}));
+const itemsLeft = [
+  {
+    src: img1,
+    size: 'big',
+  },
+  {
+    src: img2,
+    size: 'small',
+  },
+  {
+    src: img3,
+    size: 'small',
+  },
+  {
+    src: img4,
+    size: 'big',
+  },
+  {
+    src: img5,
+    size: 'small',
+  },
+  {
+    src: img6,
+    size: 'small',
+  },
+];
+
+const itemsRight = [
+  {
+    src: img8,
+    size: 'small',
+  },
+  {
+    src: img9,
+    size: 'small',
+  },
+  {
+    src: img7,
+    size: 'big',
+  },
+  {
+    src: img11,
+    size: 'small',
+  },
+  {
+    src: img12,
+    size: 'small',
+  },
+  {
+    src: img10,
+    size: 'big',
+  },
+];
 
 export default function Gallery() {
   return (
     <Layout>
       <section className="text-gray-600 body-font">
-        <div className="container px-5 py-24 mx-auto">
-          <div className="flex flex-wrap w-full mb-20">
-            <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
-              <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">
-                Pitchfork Kickstarter Taxidermy
-              </h1>
-              <div className="h-1 w-20 bg-indigo-500 rounded"></div>
-            </div>
-            <p className="lg:w-1/2 w-full leading-relaxed text-gray-500">
-              Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
-              gentrify, subway tile poke farm-to-table. Franzen you probably
-              haven't heard of them man bun deep jianbing selfies heirloom prism
-              food truck ugh squid celiac humblebrag.
-            </p>
-          </div>
-          <div className="flex flex-wrap -m-4">
-            {items.map(({ id, src, title, subtitle, desc }) => (
-              <div key={id} className="xl:w-1/4 md:w-1/2 p-4">
-                <div className="bg-gray-100 p-6 rounded-lg">
+        <div className="pt-20 flex flex-wrap">
+          <div className="flex flex-wrap mx-1">
+            <div className="flex flex-wrap w-1/2">
+              {itemsLeft.map(({ src, size }, i) => (
+                <div
+                  key={i}
+                  className={`md:p-2 p-1 w-${size === 'big' ? 'full' : '1/2'}`}
+                >
                   <img
-                    className="h-40 rounded w-full object-cover object-center mb-6"
+                    alt="gallery"
+                    className="w-full object-cover h-full object-center block"
                     src={src}
-                    alt="content"
                   />
-                  <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">
-                    {subtitle}
-                  </h3>
-                  <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
-                    {title}
-                  </h2>
-                  <p className="leading-relaxed text-base">{desc}</p>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
+            <div className="flex flex-wrap w-1/2">
+              {itemsRight.map(({ src, size }, i) => (
+                <div
+                  key={i}
+                  className={`md:p-2 p-1 w-${size === 'big' ? 'full' : '1/2'}`}
+                >
+                  <img
+                    alt="gallery"
+                    className="w-full object-cover h-full object-center block"
+                    src={src}
+                  />
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </section>
